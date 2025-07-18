8e784ea30cc996f719d4cdab38bf93ea285278ed
Aly Kotb <6829@ascsdubai.ae>
2025-07-18 17:45:12 +0400

    first commit

---
+++ Assets/Scripts/player_controller.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+@export var speed = 10.0
+@export var jump_power = 10.0
+
+var speed_multiplier = 30.0
+var jump_multiplier = -30.0
+var direction = 0
+
+@onready var animated_sprite_2d = $Node2D/AnimatedSprite2D
+
+func _ready():
+	pass	
+
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0 
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_power * jump_multiplier
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed * speed_multiplier
+		animated_sprite_2d.play("run")
+		animated_sprite_2d.flip_h = true if direction < 0 else false
+
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
+		animated_sprite_2d.play("idle")
+	
+	
+	move_and_slide()
